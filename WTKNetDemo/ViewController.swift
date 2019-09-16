b78228cd38fe8377cf4d7344c46d17549136a511
wangtk <wangtk@xh-invest.com>
2019-09-16 10:29:24 +0800

    Initial Commit

---
+++ WTKNetDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WTKNetDemo
+//
+//  Created by 王同科 on 2019/9/16.
+//  Copyright © 2019 王同科. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
